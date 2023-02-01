26a083f50ac0b6afa76f5443dff234a31d16b84e
ammhafi <akhlish.muhib@gmail.com>
2023-02-01 16:43:33 +0700

    project added

--- src/pages/projects/ProjectsData.js
+++ src/pages/projects/ProjectsData.js
@@ -3,6 +3,7 @@ import projectImg2 from '../../assets/calc.PNG'
 import projectImg3 from '../../assets/etuku.PNG'
 import projectImg4 from '../../assets/gamepage.PNG'
 import projectImg5 from '../../assets/syahrutravel.PNG'
+import projectImg6 from '../../assets/maicub.PNG'
 
 const ProjectsData = [
     {
@@ -47,11 +48,11 @@ const ProjectsData = [
     },
     {
         id: 6,
-        cover: projectImg3,
-        name: "To Be Updated",
-        category: "product",
-        desc: "Need to be updated",
-        url: 'https://etuku.vercel.app/',
+        cover: projectImg6,
+        name: "Maicub",
+        category: "web",
+        desc: "Maicub is personal website made by cloning youtube app using MERN stack",
+        url: 'https://maicub.vercel.app/',
     },
 
 ]
